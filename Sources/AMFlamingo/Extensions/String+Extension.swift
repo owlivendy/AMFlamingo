bbc64b84b950caee26a2d2ecd5b3c69807b6a981
owlivendy <owliveny@gmail.com>
2025-11-14 14:51:42 +0800

    feat: 添加扩展方法

---
+++ Sources/AMFlamingo/Extensions/String+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  String+Extension.swift
+//  AMFlamingo
+//
+//  Created by meotech on 2025/11/12.
+//
+
+import Foundation
+
+enum FlagSize {
+    case size_40x30
+    
+    var size: (width:Int, height:Int) {
+        switch self {
+        case .size_40x30:
+            return (40, 30)
+        }
+    }
+}
+
+extension String {
+    
+    func createCoutryIconUrl(with flagSize: FlagSize = .size_40x30, ext: String = "png") -> String {
+        return "https://flagcdn.com/\(flagSize.size.width)x\(flagSize.size.height)/\(self.lowercased()).\(ext)"
+    }
+    
+}
